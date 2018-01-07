fb596524eaebe3b1c8f4adc2a0a692eff1cfec23
Yannik Sembritzki <yannik@sembritzki.me>
2018-01-07 20:52:43 +0100

    Detect python path from /etc/config/qpkg.conf

--- renew_certificate.sh
+++ renew_certificate.sh
@@ -5,6 +5,15 @@ set -e
 echo "Checking whether to renew certificate on $(date -R)"
 [ -s letsencrypt/signed.crt ] && openssl x509 -noout -in letsencrypt/signed.crt -checkend 2592000 && exit
 
+if /sbin/getcfg Python Install_Path -f /etc/config/qpkg.conf; then
+    PYTHON="$(/sbin/getcfg Python Install_Path -f /etc/config/qpkg.conf)/bin/python2"
+elif /sbin/getcfg Python3 Install_Path -f /etc/config/qpkg.conf; then
+    PYTHON="$(/sbin/getcfg Python3 Install_Path -f /etc/config/qpkg.conf)/python3/bin/python3"
+else
+    echo "Error: You need to install the python 2.7 or 3.5 qpkg!"
+    exit 1
+fi
+
 echo "Renewing certificate..."
 echo "Stopping Qthttpd hogging port 80.."
 
@@ -12,13 +21,13 @@ echo "Stopping Qthttpd hogging port 80.."
 
 mkdir -p tmp-webroot/.well-known/acme-challenge
 cd tmp-webroot
-python ../HTTPServer.py &
+"$PYTHON" ../HTTPServer.py &
 pid=$!
 cd ..
 echo "Started python HTTP server with pid $pid"
 
 export SSL_CERT_FILE=cacert.pem
-python acme-tiny/acme_tiny.py --account-key letsencrypt/account.key --csr letsencrypt/domain.csr --acme-dir tmp-webroot/.well-known/acme-challenge > letsencrypt/signed.crt
+"$PYTHON" acme-tiny/acme_tiny.py --account-key letsencrypt/account.key --csr letsencrypt/domain.csr --acme-dir tmp-webroot/.well-known/acme-challenge > letsencrypt/signed.crt
 echo "Downloading intermediate certificate..."
 wget --no-verbose -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > letsencrypt/intermediate.pem
 cat letsencrypt/signed.crt letsencrypt/intermediate.pem > letsencrypt/chained.pem
