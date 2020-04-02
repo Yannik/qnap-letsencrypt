41a2bd1a50fd6514e9dd632a4ace6ee09030990c
Yannik Sembritzki <yannik@sembritzki.me>
2020-04-02 21:23:41 +0200

    Remove obsolete try block, fixes #76

--- HTTPServer.py
+++ HTTPServer.py
@@ -10,18 +10,17 @@ def main():
     serve()
   except OSError as e:
     if e.errno == errno.EAFNOSUPPORT: # system doesn't support ipv6
+      print("Serving v4 only")
       servev4only()
     else:
       raise
   
 # serves both v4 and v6
 def serve():
-  try:
-    server = HTTPServerV6(('::', 80), SimpleHTTPRequestHandler)
-    server.serve_forever()
+  server = HTTPServerV6(('::', 80), SimpleHTTPRequestHandler)
+  server.serve_forever()
  
 def servev4only():
-  print("Serving v4 only")
   server = HTTPServer(('0.0.0.0', 80), SimpleHTTPRequestHandler)
   server.serve_forever()
 
