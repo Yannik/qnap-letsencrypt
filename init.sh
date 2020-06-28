2291cd1141b60bc6dd84e9d3edb9590d4e29c034
Yannik Sembritzki <yannik.sembritzki@gmail.com>
2020-06-28 15:19:46 +0200

    Remove never used -sha256 openssl genrsa flag
    It is already specified correctly in the `openssl req` command in the README

--- init.sh
+++ init.sh
@@ -3,5 +3,4 @@ git clone https://github.com/diafygi/acme-tiny.git
 mkdir -p letsencrypt/keys
 cd letsencrypt
 openssl genrsa 4096 > account.key
-openssl genrsa -out keys/domain.key 2048 -sha256
-
+openssl genrsa -out keys/domain.key 2048
