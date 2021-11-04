dc895ec3ef8cc537cfafb11431d8304d1236a1f8
TIK <TIK@abandonedsources.com>
2021-11-04 01:17:03 +0100

    Reverted changes of HTTPServer

--- HTTPServer.py
+++ HTTPServer.py
@@ -1,4 +1,4 @@
-import socket, errno, sys
+import socket, errno
 
 from http.server import SimpleHTTPRequestHandler,HTTPServer
 
@@ -6,25 +6,22 @@ class HTTPServerV6(HTTPServer):
   address_family = socket.AF_INET6
 
 def main():
-  port = 80  # default port
-  if len(sys.argv)>1:
-    port = int(sys.argv[1])
   try:
-    serve(port)
+    serve()
   except OSError as e:
     if e.errno == errno.EAFNOSUPPORT or str(e) == 'getsockaddrarg: bad family':
       print("Serving v4 only")
-      servev4only(port)
+      servev4only()
     else:
       raise
-
+  
 # serves both v4 and v6
-def serve( port ):
-  server = HTTPServerV6(('::', port), SimpleHTTPRequestHandler)
+def serve():
+  server = HTTPServerV6(('::', 80), SimpleHTTPRequestHandler)
   server.serve_forever()
-
-def servev4only( port ):
-  server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
+ 
+def servev4only():
+  server = HTTPServer(('0.0.0.0', 80), SimpleHTTPRequestHandler)
   server.serve_forever()
 
 if __name__ == '__main__':
